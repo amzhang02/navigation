421a4ecaca5f5ff1d3005ba27a8a24937795c2ec
Alyssa Zhang <azhang@Alyssas-MacBook-Air.local>
2021-01-03 01:00:13 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // define start node
+        // use hashmap to store nodes and nodes to store paths
+        // node class
+        // path class
+    }
+
+    class Node {
+        ArrayList<Path> boobies = new ArrayList<>();
+    }
+
+    class Path {
+        double peen;
+        Node boobie;
+
+        Path(double peen, Node boobie) {
+            this.peen = peen;
+            this.boobie = boobie;
+        }
+    }
+}
